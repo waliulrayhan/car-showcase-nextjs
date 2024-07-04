6e6b953dfdb47a97145c3f1b0f210e3f208226d5
Md. Waliul Islam Rayhan <b190305034@cse.jnu.ac.bd>
2024-07-04 23:41:54 +0600

    Some Components are added.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,46 @@
-import Image from "next/image"
+"use client";
+
+import Image from "next/image";
+import CustomButton from "./CustomButton";
+
+// import { CustomButton } from "@components";
 
 const Hero = () => {
-    return (
-        <div>
-            Hero
-        </div>
-    )
-}
+    const handleScroll = () => {
+        const nextSection = document.getElementById("discover");
 
-export default Hero
+        if (nextSection) {
+            nextSection.scrollIntoView({ behavior: "smooth" });
+        }
+    };
+
+    return (
+        <div className="hero">
+            <div className="flex-1 pt-36 padding-x">
+                <h1 className="hero__title">
+                    Find, book, rent a car. Quick and super easy!
+                </h1>
+
+                <p className="hero__subtitle">
+                    Streamline your car rental experience with our effortless booking
+                    process.
+                </p>
+
+                <CustomButton
+                    title="Explore Cars"
+                    containerStyles="bg-primary-blue text-white rounded-full mt-10"
+                    handleClick={handleScroll}
+                />
+            </div>
+            <div className="hero__image-container">
+                <div className="hero__image">
+                    <Image src="/hero.png" alt="hero" fill className="object-contain" />
+                </div>
+
+                <div className="hero__image-overlay" />
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
